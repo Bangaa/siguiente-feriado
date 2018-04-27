05979d95a340cba1556250eb3c1b8116e29d9617
Ian Mejias <idmconejeros@gmail.com>
2018-04-27 19:02:39 -0300

    Intento de corregir integracion de Tracis y Heroku

--- feriados_chile/settings.py
+++ feriados_chile/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+## Production state
+
+IN_PRODUCTION = True if os.environ.get('DJANGO_IN_PRODUCTION', default=None) else False
+print("PRODUCTION STATE --> {!s}".format(IN_PRODUCTION))
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,10 +25,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DJANGO_DEBUG', False)
+DEBUG = False if IN_PRODUCTION else True
 
 ALLOWED_HOSTS = []
 
@@ -122,12 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
 
-# Facil configuracion del proyecto en Heroku
-
-import django_heroku
-django_heroku.settings(
-    locals(),
-    secret_key=False
-)
+if IN_PRODUCTION:
+    import django_heroku
+    django_heroku.settings(
+        locals(),
+        secret_key=False
+    )
