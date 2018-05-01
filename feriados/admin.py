6ac5ecaf357141101858f15d8436c198a356663a
Ian Mejias <idmconejeros@gmail.com>
2018-05-01 14:51:38 -0300

    Se implemeta forma de agregar feriados

--- feriados/admin.py
+++ feriados/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Feriado
 
-# Register your models here.
+@admin.register(Feriado)
+class FeriadoAdmin(admin.ModelAdmin):
+    list_display = ('fecha', 'festividad', 'es_irrenunciable',)
